dffa9a13273bc53b14c1aa42f1151f03be37c2e9
wakenedo <a_nuner@outlook.com>
2022-01-25 11:35:57 -0300

    Part 3 - Challenge Complete
    App:
    Components:
    -Food
    -Header
    -Modal
    -ModalAddFood
    
    All Components refactor to TypeScript Syntax and files to its respective extensions. App working as intended with added types and more modern code structure. Following the same principles as the first commits to structure components.

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import { FiPlusSquare } from "react-icons/fi";
+import Logo from "../../assets/logo.svg";
+import { Container } from "./styles";
+
+type HeaderProps = {
+  openModal: () => void;
+};
+
+const Header = ({ openModal }: HeaderProps) => (
+  <Container>
+    <header>
+      <img src={Logo} alt="GoRestaurant" />
+      <nav>
+        <div>
+          <button type="button" onClick={openModal}>
+            <div className="text">Novo Prato</div>
+            <div className="icon">
+              <FiPlusSquare size={24} />
+            </div>
+          </button>
+        </div>
+      </nav>
+    </header>
+  </Container>
+);
+
+export default Header;
--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,3 +1,4 @@
 declare module 'react-router-dom'
 declare module 'styled-components'
 declare module 'axios'
+declare module 'react-modal'
